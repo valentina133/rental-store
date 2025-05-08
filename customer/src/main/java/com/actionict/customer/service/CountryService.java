236f4371056ec3f478b19051350d6bdc8955bd59
ACTIONICT\ActionICT <va.giordano@reply.it>
2025-05-08 09:46:14 +0200

    CountryService

--- customer/src/main/java/com/actionict/customer/service/CountryService.java
+++ customer/src/main/java/com/actionict/customer/service/CountryService.java
@@ -16,4 +16,26 @@ public class CountryService {
     public List<Country> findAll() {
         return countryRepository.findAll();
     }
+
+    //Trova Uno
+    public Country findById(Integer id) {
+
+        return countryRepository.findById(id);
+    }
+
+    //Inserisci
+    public void inserisci(String name) {
+        Country country=new Country (name);
+        countryRepository.save(country);
+    }
+
+    //Aggiorna
+    public void Update(Integer id, String newName) {
+        Country country=countryRepository.findById(id);
+        country.setName(newName);
+        countryRepository.save(country);
+    }
+
+    //Elimina
+    public void deleteById(Integer id) { countryRepository.deleteById(id); }
 }
