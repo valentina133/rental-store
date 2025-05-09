a1900c90d3b43b51bfd96c2450c453713a2c09e1
ACTIONICT\ActionICT <va.giordano@reply.it>
2025-05-09 11:37:22 +0200

    Service e Controller

--- customer/src/main/java/com/actionict/customer/repository/CustomerRepository.java
+++ customer/src/main/java/com/actionict/customer/repository/CustomerRepository.java
@@ -4,4 +4,6 @@ import com.actionict.customer.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    List<Customer> findByFirstName(String firstName);
+    List<Customer> findByLastName(String lastName);
 }
