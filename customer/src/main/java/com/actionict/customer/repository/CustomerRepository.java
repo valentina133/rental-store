9a9dacf44a5f2357e65801bc9df23ad2be16cfe5
ACTIONICT\ActionICT <va.giordano@reply.it>
2025-05-09 18:15:50 +0200

    Service e Controller

--- customer/src/main/java/com/actionict/customer/repository/CustomerRepository.java
+++ customer/src/main/java/com/actionict/customer/repository/CustomerRepository.java
@@ -1,9 +1,12 @@
 package com.actionict.customer.repository;
 
 import com.actionict.customer.model.Customer;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    List<Customer> findByFirstName(String firstName);
-    List<Customer> findByLastName(String lastName);
+    List<Customer> findByFirstName(String firstName, Pageable pageable);
+    List<Customer> findByLastName(String lastName, Pageable pageable);
 }
