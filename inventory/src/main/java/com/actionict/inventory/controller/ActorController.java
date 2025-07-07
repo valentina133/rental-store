c7a0782c365ebcc00b83f4f780d901c67fcbe46a
ACTIONICT\ActionICT <va.giordano@reply.it>
2025-07-07 10:48:06 +0200

    Service e Controller

--- inventory/src/main/java/com/actionict/inventory/controller/ActorController.java
+++ inventory/src/main/java/com/actionict/inventory/controller/ActorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categories")
+@RequestMapping("/actors")
 @RequiredArgsConstructor
 public class ActorController {
 
