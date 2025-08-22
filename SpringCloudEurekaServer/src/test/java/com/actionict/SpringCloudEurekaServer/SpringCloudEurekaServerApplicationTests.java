9bad21a6fab002583ef60245984064fb1d4be82d
ACTIONICT\ActionICT <va.giordano@reply.it>
2025-08-22 13:45:05 +0200

    Service e Controller

---
+++ SpringCloudEurekaServer/src/test/java/com/actionict/SpringCloudEurekaServer/SpringCloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actionict.SpringCloudEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
